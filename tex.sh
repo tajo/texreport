178457a4808cb84bde43a7c01bfc97148a592264
tajo <vojtech@miksu.cz>
2014-12-17 18:53:21 +0400

    gitignore is now created with a new report

--- tex.sh
+++ tex.sh
@@ -3,4 +3,5 @@
 mkdir $1
 cp /Users/miksu/icloud/Scripts/lab_report/lab_report.tex $1/$1.tex
 cp /Users/miksu/icloud/Scripts/lab_report/ref.bib $1/ref.bib
+cp /Users/miksu/icloud/Scripts/lab_report/.gitignore $1/.gitignore
 echo "===> LaTex project $1 has been created."
